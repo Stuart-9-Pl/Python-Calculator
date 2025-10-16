4d6ce000fdaf5686fa8e9e25b7a381a193e256a1
Stuart-9-Pl <playstuart9acc@gmail.com>
2025-10-17 01:59:16 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+    print("5 + 3 =", add(5, 3))
+    print("5 / 0 =", divide(5, 0))
